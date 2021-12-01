d5152e03619587350329d41e72abf8c7de3e6a26
Binabh <binabhdevkota@gmail.com>
2021-12-01 21:48:21 +0545

    adds fastlane

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Based Cooking',
       theme: primaryTheme,
       home: const MyHomePage(),
